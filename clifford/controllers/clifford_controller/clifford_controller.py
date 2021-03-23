e4c07fd29f2846a508b50ae19cdfa492a821cf17
Victor Babineau <vbabi020@uottawa.ca>
2021-03-23 16:06:34 -0400

    Fixed floors, Added Sensors to new robot Started working with sensors Vel and Pos are now variables

--- clifford/controllers/clifford_controller/clifford_controller.py
+++ clifford/controllers/clifford_controller/clifford_controller.py
@@ -3,41 +3,59 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot,Motor
+from vectors import Point, Vector
+import random
 
-TIME_STEP = 64
-MAX_SPEED = 100 #The robot will cap out at its max velocity:(
 
-# create the Robot instance.
+TIME_STEP = 64 #time in ms
+
+#Create Robot
 robot = Robot()
+leftMotor = robot.getDevice('A') #get motor pointers
+rightMotor = robot.getDevice('B')
+MAX_SPEED = rightMotor.getMaxVelocity() #get max speed
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+gps = robot.getDevice('gps') # get gps
+gps.enable(TIME_STEP) # Enable gps
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
-leftMotor = robot.getDevice('left wheel motor')
-rightMotor = robot.getDevice('right wheel motor')
+comp = robot.getDevice('compass') #get compasss
+comp.enable(TIME_STEP)
 
+
+emitter= robot.getDevice('emitter')
+#This should be changed in terms of breaking time
+emitter.setRange(MAX_SPEED*1000*TIME_STEP)
+
+
+
+#important variables
+speed = MAX_SPEED*random.random() #Sets speed to a random fraction of the max
+dirVect = comp.getValues() #normalized direction vector
+coords = Point.from_list(gps.getValues())      #coordinates
+velocity = Vector(dirVect[0]*speed,dirVect[1]*speed,dirVect[2]*speed)        #Velocity vector
+destination =   
+#Initial Conditions?
 leftMotor.setPosition(float('inf'))#sets the servo? to infinity so it never stops
 rightMotor.setPosition(float('inf'))
-leftMotor.setVelocity(1 * MAX_SPEED)
-rightMotor.setVelocity(1 * MAX_SPEED)
+leftMotor.setVelocity(1* MAX_SPEED)
+rightMotor.setVelocity(1* MAX_SPEED)
+
+print(robot.getCustomData)
 
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(TIME_STEP) != -1: #infinite loop cause TIME_STEP never changing
     # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
+    coords = Point.from_list(gps.getValues())  
+    #print(f"X:{round(x*100,1)} cm\nY:{round(y*100,1)} cm\nZ:{round(z*100,1)} cm")
+   
+    #for right now we only send in the position and velocity
+    emitter.send(coords,velocity)
+    #eventually we'd add the path
     # Process sensor data here.
 
     # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
     pass
 
 # Enter here exit cleanup code.
